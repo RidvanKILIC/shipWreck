48035de1c195fc41fcea915f3fc91c70f62430fd
RidvanKILIC <84662580+RidvanKILIC@users.noreply.github.com>
2024-01-11 13:05:51 +0300

    Turning animation added
    Turning animation added two animations can play simultaneously
    Leantween package installed

--- Assets/Scripts/Player/shipAnimator.cs
+++ Assets/Scripts/Player/shipAnimator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class shipAnimator : MonoBehaviour
 {
     Animator _anim;
+    [SerializeField] GameObject turnObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,33 @@ public class shipAnimator : MonoBehaviour
     }
     public void turningAnim(float _hor)
     {
+        Debug.Log("Horizontal value: " + _hor);
+        //if (_hor > 0f)
+        //{
+        //    if (turnObj.transform.rotation.y != 1400f)
+        //    {
+        //        //LeanTween.rotateY(turnObj, 140f, 1f).setEase(LeanTweenType.easeInCubic);
+        //        LeanTween.value(turnObj, turnObj.transform.rotation.y, 140f, 0.5f).setOnUpdate((a) => turnObj.transform.rotation = Quaternion.Euler(0f, a, 0f)).setEase(LeanTweenType.linear);
+        //    }
+        //}
+        //else if (_hor < 0f)
+        //{
+        //    if (turnObj.transform.rotation.y != 70f)
+        //    {
+        //        //LeanTween.rotateY(turnObj, 70f, 1f).setEase(LeanTweenType.easeInCubic);
+        //        LeanTween.value(turnObj, turnObj.transform.rotation.y, 70f, 0.5f).setOnUpdate((a) => turnObj.transform.rotation = Quaternion.Euler(0f, a, 0f)).setEase(LeanTweenType.linear);
+        //    }
+               
+        //}
+        //else
+        //{
+        //    if(turnObj.transform.rotation.y != 90f)
+        //    {
+        //        //LeanTween.rotateY(turnObj,90f, 1f).setEase(LeanTweenType.easeInCubic);
+        //        LeanTween.value(turnObj, turnObj.transform.rotation.y, 90f, 0.5f).setOnUpdate((a) => turnObj.transform.rotation = Quaternion.Euler(0f, a, 0f)).setEase(LeanTweenType.linear);
+        //    }
+           
+        //}
         _anim.SetFloat("Horizontal", _hor);
     }
     public void idleAnim(bool _idle)
